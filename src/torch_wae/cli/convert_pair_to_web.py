31d1245dff8583f0d91272109adff2f5433015af
Tomoya Kose <tomoya@mitsuse.jp>
2024-11-18 11:27:45 +0900

    Revert "Convert metadata and mel-spec separately."
    This reverts commit fcd82b59ab045d1b4c90a2576a73bf7252e56fb6.

--- src/torch_wae/cli/convert_pair_to_web.py
+++ src/torch_wae/cli/convert_pair_to_web.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Any
 
+import torch
+import torchaudio
 import typer
+from torchaudio import functional as FA
 
 app = typer.Typer()
 
@@ -23,12 +26,12 @@ def main(
         help="the path of a dataset to be encoded.",
     ),
     size_shard: int = typer.Option(
-        20,
+        200,
         help="the max size of a shard (unit: MB)",
     ),
     output: Path = typer.Option(
         ...,
-        help="the output path of a directory which stores shards of WebDataset.",
+        help="the output path of a directory which stores shards of WebDataset,",
     ),
 ) -> None:
     from torch.utils.data import DataLoader
@@ -46,7 +49,11 @@ def main(
     loader = DataLoader(
         transformed_iter(
             dataset,
-            Transform(),
+            Transform(
+                root=root,
+                resample_rate=16000,
+                durations=1,
+            ),
         ),
         batch_size=1,
         shuffle=False,
@@ -59,18 +66,26 @@ def main(
 
     with tqdm(total=n) as progress:
         with ShardWriter(pattern, maxsize=max_size, verbose=0) as w:
-            for i, (anchor, positive, key, class_id) in enumerate(loader):
+            for i, (anchor, positive, key, class_id, melspec, ignore) in enumerate(
+                loader
+            ):
                 if n <= i:
                     break
+
+                ignore = bool(ignore.detach().numpy()[0])
+                if ignore:
+                    continue
 
                 anchor = anchor[0]
                 positive = positive[0]
                 key = key[0]
                 class_id = int(class_id.detach().numpy()[0])
+                melspec = melspec.detach().numpy()[0]
 
                 w.write(
                     {
                         "__key__": key,
+                        "npy": melspec,
                         "json": {
                             "class_id": class_id,
                             "anchor": anchor,
@@ -83,10 +98,25 @@ def main(
 
 
 class Transform:
-    def __init__(self) -> None: ...
+    def __init__(
+        self,
+        root: Path,
+        resample_rate: int,
+        durations: int,
+    ) -> None:
+        from torch_wae.network import Preprocess
 
-    def __call__(self, example: Any) -> tuple[str, str, str, int]:
+        super().__init__()
+
+        self.__root = root
+        self.__resample_rate = resample_rate
+        self.__durations = durations
+        self.__preprocess = Preprocess()
+
+    def __call__(self, example: Any) -> tuple[str, str, str, int, torch.Tensor, bool]:
         from torch_wae import fs
+
+        root = self.__root
 
         anchor = Path(example["anchor"])
         positive = Path(example["positive"])
@@ -95,13 +125,38 @@ class Transform:
         basename_anchor = fs.basename(anchor)
         basename_positive = fs.basename(positive)
 
-        path_anchor = anchor.parent / f"{basename_anchor}.npy"
-        path_positive = positive.parent / f"{basename_positive}.npy"
-
         key = str(path / f"{basename_anchor}-{basename_positive}")
         class_id = int(example["class_id"])
 
-        return str(path_anchor), str(path_positive), key, class_id
+        mel_anchor, ignore_anchor = self.convert_to_melspec(root / anchor)
+        mel_positive, ignore_positive = self.convert_to_melspec(root / positive)
+        melspec = torch.stack((mel_anchor, mel_positive))
+
+        ignore = ignore_anchor or ignore_positive
+
+        return str(anchor), str(positive), key, class_id, melspec, ignore
+
+    def convert_to_melspec(self, path: Path) -> tuple[torch.Tensor, bool]:
+        from torch_wae.audio import crop_or_pad_last
+
+        resample_rate = self.__resample_rate
+        durations = self.__durations
+        preprocess = self.__preprocess
+
+        waveform, sample_rate = torchaudio.load(path)
+        waveform = torch.mean(waveform, dim=0).unsqueeze(0)
+
+        if resample_rate != sample_rate:
+            waveform = FA.resample(waveform, sample_rate, resample_rate)
+
+        frames = waveform.shape[-1]
+
+        waveform = crop_or_pad_last(resample_rate, durations, waveform)
+        melspec = preprocess(waveform)[0]
+
+        ignore = frames > durations * resample_rate
+
+        return melspec, ignore
 
 
 if __name__ == "__main__":
